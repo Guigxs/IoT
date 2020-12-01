b2ceb62a8066449c646c7900596188b7aeb1103d
Guigxs <guigxs19@gmail.com>
2020-12-01 16:05:33 +0100

    Merge thomas version

--- main.py
+++ main.py
@@ -3,49 +3,84 @@ import ubinascii
 import socket
 import time
 import cayenneLPP
+import machine
+from machine import Pin
+from math import pi
 
 lora = LoRa()
 print("DevEUI: %s" % (ubinascii.hexlify(lora.mac()).decode('ascii')))
 
 lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
 
-# create an OTAA authentication parameters, change them to the provided credentials
 app_eui = ubinascii.unhexlify('70B3D57ED003839F')
-app_key = ubinascii.unhexlify('C81BC3EF1F934DD512ECF2C0FD99AF0B')
+app_key = ubinascii.unhexlify('69CD1693FF4AF4B5B508B62DE90FD06C')
 
-# join a network using OTAA (Over the Air Activation)
-#uncomment below to use LoRaWAN application provided dev_eui
 lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
-#lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 
-# wait until the module has joined the network
+
+adc = machine.ADC()             # create an ADC object
+apin = adc.channel(pin='P16')   # create an analog pin on P16
+val = apin()                    # read an analog value
+
+if( 3600 <= val):
+    # print("Nord")
+    direction = 1
+
+elif( 800 < val < 1500):
+    # print("Est")
+    direction = 2
+
+
+elif(3000 < val < 3600):
+    # print("Sud")
+    direction = 3
+
+else:
+    # print("Error")
+    direction = -1
+# elif(2500 < val < 5000):
+#     print("Est")
+#     direction = 2
+
+
+beginTime = time.time()
+actualTime = time.time()
+flancMontant = 0
+flanMontantexcecute = False
+pin = Pin('P12', mode=Pin.IN, pull=Pin.PULL_DOWN)
+
+while( beginTime + 5 > actualTime):
+    pine = pin()   # fast method to get the value
+    if (pine ==0):
+        flanMontantexcecute = False
+    
+    if(flanMontantexcecute == False):
+        if( pine ==1):
+            flancMontant +=1
+            flanMontantexcecute = True
+    actualTime = time.time()
+
+vitesse = (flancMontant/5) * 2 * pi * 0.07
+
+
 while not lora.has_joined():
     time.sleep(2.5)
     print('Not yet joined...')
 
 print('Joined')
 
-# create a LoRa socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-
-# set the LoRaWAN data rate
 s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 
-# make the socket blocking
-# (waits for the data to be sent and for the 2 receive windows to expire)
 s.setblocking(True)
 
+# print(direction)
+# print(vitesse)
 lpp = cayenneLPP.CayenneLPP(size = 100, sock = s)
-lpp.add_analog_input(34, 4)
-lpp.add_analog_input(133)
-lpp.add_gps(51.434343, 7.545454, 0)
+lpp.add_analog_input(direction, 4)
+lpp.add_analog_input(vitesse)
+lpp.add_gps(40.434343, 8.545454, 0)
 lpp.set_socket(s)
 lpp.send()
 
-# make the socket non-blocking
-# (because if there's no data received it will block forever...)
 s.setblocking(False)
-
-# get any data received (if any...)
-data = s.recv(64)
-print(data)
